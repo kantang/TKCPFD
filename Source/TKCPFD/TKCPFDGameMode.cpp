febfda3478b7c51405861e474a61d3bc8b607d7d
Kan Tang <kantangcn@gmail.com>
2015-08-16 23:42:44 +0100

    Base Code
    The base code for a blank unreal project

---
+++ Source/TKCPFD/TKCPFDGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TKCPFD.h"
+#include "TKCPFDGameMode.h"
+
+
+
+
---
+++ Source/TKCPFD/TKCPFDGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TKCPFDGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TKCPFD_API ATKCPFDGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
